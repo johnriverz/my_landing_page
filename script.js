678e6c089d29c2e1fd6bb0941bc00b17d4de377a
johnriverz <juan.jose.rios.jr@gmail.com>
2024-07-29 21:30:04 -0700

    Make a functional contact form
    Uses emailJS to send messages to my email

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    let params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    }
+
+    // send to emailJS server
+    emailjs.send("service_5n6lzws", "template_avejjvk", params).then(alert("Email has been sent"))
+}
